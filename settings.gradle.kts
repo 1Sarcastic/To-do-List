531ce3c37e3584599b83b1016c66950b03d6a1c7
1Sarcastic <156623367+1Sarcastic@users.noreply.github.com>
2024-11-22 19:52:37 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS) // Allow the settings repositories to take priority
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "To do App"
+include(":app")
